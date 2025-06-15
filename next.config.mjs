2188dd6fca0939a638d198a5990e405bd4e64a6b
Raghu K <125834546+Orbit17@users.noreply.github.com>
2025-06-15 00:31:27 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = {
+const nextConfig = {
+  reactStrictMode: true,
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // or true if you want to bypass ESLint build errors
   },
-}
+  images: {
+    domains: ['your-image-domain.com'], // optional, remove if unused
+  },
+  // other config options here
+};
 
 export default nextConfig;
